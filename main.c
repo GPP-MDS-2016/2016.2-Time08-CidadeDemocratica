053b6b632ec1f4001cfa1ced7d7c834ca06dfe1b
luisresende <luisfrvilela@gmail.com>
2016-08-18 20:51:27 -0300

    Adicionando Mensagem de erro e chamando funcao add

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
 void printMenu();
+void printInvalidOptionMessage();
 int add(int a, int b);
 
 int main() {
+
+  printMenu();
+  int option = 0;
+
+  scanf("%d", &option);
+
+  int numA = 0, numB = 0;
+  int result = 0;
+
+  do {
+    switch (option) {
+      case 1:
+        scanf("%d%d\n", &numA, &numB);
+        result = add(numA, numB);
+        break;
+      default:
+        printInvalidOptionMessage();
+    }
+  } while (option != 1);
+  printf("%d\n",result );
 
   return 0;
 }
@@ -14,4 +35,8 @@ int add(int a, int b) {
 
 void printMenu() {
   printf("1. Add\n");
+}
+
+void printInvalidOptionMessage() {
+  printf("Escolha uma opcao valida\n");
 }
