3df6dc4e20b2cb5b008ebd82076210af14ca6a00
luisresende <luisfrvilela@gmail.com>
2016-08-18 21:10:52 -0300

    Atualizado switch de opcoes de operacoes

--- main.c
+++ main.c
@@ -1,55 +1,61 @@
-#include <stdio.h>
+  #include <stdio.h>
 
-void printMenu();
-void printInvalidOptionMessage();
-int add(int a, int b);
-int sub(int numA,int numB);
-int div(int numA, int numB);
-int main() {
+  void printMenu();
+  void printInvalidOptionMessage();
+  int add(int a, int b);
+  int sub(int numA, int numB);
+  int div(int numA, int numB);
 
-  printMenu();
-  int option = 0;
+  int main() {
 
-  scanf("%d", &option);
+    printMenu();
+    int option = 0;
 
-  int numA = 0, numB = 0;
-  int result = 0;
+    scanf("%d", &option);
 
-  do {
-    switch (option) {
-      case 1:
-        scanf("%d%d\n", &numA, &numB);
-        result = add(numA, numB);
-        break;
-     case 2:
-        scanf("%d%d",&numA,&numB);
-        result = sub(numA,numB);
-        break;
-      default:
-        printInvalidOptionMessage();
-    }
-  } while (option > 2 || option < 1);
-  printf("%d\n",result );
+    int numA = 0, numB = 0;
+    int result = 0;
 
-  return 0;
-}
+    do {
+      switch (option) {
+        case 1:
+          scanf("%d%d\n", &numA, &numB);
+          result = add(numA, numB);
+          break;
+       case 2:
+          scanf("%d%d", &numA, &numB);
+          result = sub(numA, numB);
+          break;
+      case 3:
+          scanf("%d%d", &numA, &numB);
+          result = div(numA, numB);
+        default:
+          printInvalidOptionMessage();
+      }
+    } while (option > 2 || option < 1);
+    printf("%d\n", result);
 
-int add(int a, int b) {
-  return a + b;
-}
+    return 0;
+  }
 
-void printMenu() {
-  printf("1. Add\n2. Sub\n");
-}
+  int add(int a, int b) {
+    return a + b;
+  }
 
-void printInvalidOptionMessage() {
-  printf("Escolha uma opcao valida\n");
-}
- int sub(int numA,int numB){
-   int result = numA - numB;
-   return result;
- }
-int div(int numA, int numB){
-  int result  = numA / numB;
-  return result;
-}
+  void printMenu() {
+    printf("1. Add\n2. Sub\n");
+  }
+
+  void printInvalidOptionMessage() {
+    printf("Escolha uma opcao valida\n");
+  }
+
+  int sub(int numA, int numB) {
+    int result = numA - numB;
+    return result;
+  }
+
+  int div(int numA, int numB) {
+    int result  = numA / numB;
+    return result;
+  }
